7b68304834bb87a5c8421a804395b81cf0e2b990
Raide Junior <raidejunior@gmail.com>
2024-10-23 03:10:58 -0300

    update

--- src/br/com/alura/minhasmusicas/MinhasPreferidas.java
+++ src/br/com/alura/minhasmusicas/MinhasPreferidas.java
@@ -4,7 +4,7 @@ public class MinhasPreferidas {
 
     public void inclui(Audio audio){
         if(audio.getClassificacao() >= 9) {
-            System.out.println(audio.getTitulo() + "é considerado sucesso absoluto " + "e preferido por todos!");
+            System.out.println(audio.getTitulo() + " é considerado sucesso absoluto " + "e preferido por todos!");
         } else {
             System.out.println(audio.getTitulo() + " também é um dos que todo mundo está " + "curtindo");
         }
