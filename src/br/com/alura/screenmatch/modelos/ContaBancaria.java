5d44b8c5e6add993caf006d1d767d71ca52dec03
Raide Junior <raidejunior@gmail.com>
2024-10-27 19:20:35 -0300

    update

---
+++ src/br/com/alura/screenmatch/modelos/ContaBancaria.java
@@ -0,0 +1,19 @@
+package br.com.alura.screenmatch.modelos;
+
+public class ContaBancaria {
+    private int numero;
+    private double saldo;
+
+    public ContaBancaria(int numero, double saldo) {
+        this.numero = numero;
+        this.saldo = saldo;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+}
