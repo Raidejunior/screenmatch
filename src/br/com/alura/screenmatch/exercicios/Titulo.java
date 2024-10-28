1cf010a92297b139a7fc4cf8b4a6e5446da6e66a
Raide Junior <raidejunior@gmail.com>
2024-10-28 08:16:44 -0300

    update

---
+++ src/br/com/alura/screenmatch/exercicios/Titulo.java
@@ -0,0 +1,26 @@
+package br.com.alura.screenmatch.exercicios;
+
+public class Titulo implements Comparable<Titulo> {
+    private String nome;
+
+    public Titulo(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+
+    @Override
+    public int compareTo(Titulo outroTitulo) {
+        return this.getNome().compareTo(outroTitulo.getNome());
+    }
+
+    @Override
+    public String toString() {
+        return String.format("""
+                Nome do Titulo: %s,
+                """, this.getNome());
+    }
+}
