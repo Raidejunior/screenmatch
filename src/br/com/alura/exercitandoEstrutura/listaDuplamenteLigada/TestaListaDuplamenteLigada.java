704d63ff1d10ff20f9fe91778c8e5f8cfaefd170
Raide Junior <raidejunior@gmail.com>
2024-12-02 11:49:24 -0300

    update

---
+++ src/br/com/alura/exercitandoEstrutura/listaDuplamenteLigada/TestaListaDuplamenteLigada.java
@@ -0,0 +1,34 @@
+package br.com.alura.exercitandoEstrutura.listaDuplamenteLigada;
+
+
+public class TestaListaDuplamenteLigada {
+    public static void main(String[] args) {
+        ListaDuplamenteLigada ListaDuplamenteLigada = new ListaDuplamenteLigada();
+
+
+        System.out.println(ListaDuplamenteLigada);
+        ListaDuplamenteLigada.adicionaNoComeco("Raide");
+        System.out.println(ListaDuplamenteLigada);
+        ListaDuplamenteLigada.adicionaNoComeco("Rafaela");
+        System.out.println(ListaDuplamenteLigada);
+        ListaDuplamenteLigada.adicionaNoComeco("Maísa");
+        System.out.println(ListaDuplamenteLigada);
+        ListaDuplamenteLigada.adiciona("Fulano");
+        System.out.println(ListaDuplamenteLigada);
+        ListaDuplamenteLigada.adiciona(1, "Ciclano");
+        System.out.println(ListaDuplamenteLigada);
+
+        String maisa = ListaDuplamenteLigada.pega(0).toString();
+        System.out.println(maisa);
+
+        int tamanho = ListaDuplamenteLigada.tamanho();
+        System.out.println(tamanho);
+
+        ListaDuplamenteLigada.removeDoComeco();
+        System.out.println(ListaDuplamenteLigada);
+
+        ListaDuplamenteLigada.remove(1);
+        System.out.println(ListaDuplamenteLigada);
+
+    }
+}
