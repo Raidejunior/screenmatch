46443a28a88aadd972ade799fd1cc6959e891846
Raide Junior <raidejunior@gmail.com>
2024-12-03 16:28:07 -0300

    update

--- src/br/com/alura/screenmatch/exercicios/Titulo.java
+++ src/br/com/alura/screenmatch/exercicios/Titulo.java
@@ -1,5 +1,6 @@
 package br.com.alura.screenmatch.exercicios;
 
+import br.com.alura.screenmatch.modelos.TituloOmdb;
 import com.google.gson.annotations.SerializedName;
 
 import java.io.Serial;
@@ -13,6 +14,12 @@ public class Titulo implements Comparable<Titulo> {
     private double somaDasAvaliacoes;
     private int totalDeAvaliacoes;
     private int duracaoEmMinutos;
+
+    public Titulo(TituloOmdb meuTituloOMDB) {
+        this.nome = meuTituloOMDB.title();
+        this.anoDeLancamento = Integer.valueOf(meuTituloOMDB.year());
+        this.duracaoEmMinutos = Integer.valueOf(meuTituloOMDB.runtime().substring(0, 3));
+    }
 
     public int getAnoDeLancamento() {
         return anoDeLancamento;
@@ -32,11 +39,16 @@ public class Titulo implements Comparable<Titulo> {
         return this.getNome().compareTo(outroTitulo.getNome());
     }
 
+    public int getDuracaoEmMinutos() {
+        return duracaoEmMinutos;
+    }
+
     @Override
     public String toString() {
         return String.format("""
                 Nome do Titulo: %s,
-                Ano de lançamento: %d
-                """, this.getNome(), this.getAnoDeLancamento());
+                Ano de lançamento: %d,
+                Duração: %d
+                """, this.getNome(), this.getAnoDeLancamento(), this.getDuracaoEmMinutos());
     }
 }
