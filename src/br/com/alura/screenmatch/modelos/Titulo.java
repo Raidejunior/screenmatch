03abfd0b676697f433111ff32618339df929f8cf
raidejunior <raidejunior@gmail.com>
2025-03-26 15:26:42 -0300

    update

--- src/br/com/alura/screenmatch/modelos/Titulo.java
+++ src/br/com/alura/screenmatch/modelos/Titulo.java
@@ -1,9 +1,11 @@
 package br.com.alura.screenmatch.modelos;
 
-import java.util.Collection;
+import com.google.gson.annotations.SerializedName;
 
-public class Titulo implements Comparable<Titulo>{
+public class Titulo implements Comparable<Titulo> {
+    @SerializedName("Title")
     private String nome;
+    @SerializedName("Year")
     private int anoDeLancamento;
     private boolean incluidoNoPlano;
     private double somaDasAvaliacoes;
@@ -15,20 +17,28 @@ public class Titulo implements Comparable<Titulo>{
         this.anoDeLancamento = anoDeLancamento;
     }
 
-    public int getTotalDeAvaliacoes() {
-        return totalDeAvaliacoes;
-    }
-
     public String getNome() {
         return nome;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
     public int getAnoDeLancamento() {
         return anoDeLancamento;
+    }
+
+    public boolean isIncluidoNoPlano() {
+        return incluidoNoPlano;
+    }
+
+    public int getDuracaoEmMinutos() {
+        return duracaoEmMinutos;
+    }
+
+    public int getTotalDeAvaliacoes() {
+        return totalDeAvaliacoes;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
     }
 
     public void setAnoDeLancamento(int anoDeLancamento) {
@@ -39,31 +49,33 @@ public class Titulo implements Comparable<Titulo>{
         this.incluidoNoPlano = incluidoNoPlano;
     }
 
-    public int getDuracaoEmMinutos() {
-        return duracaoEmMinutos;
-    }
-
     public void setDuracaoEmMinutos(int duracaoEmMinutos) {
         this.duracaoEmMinutos = duracaoEmMinutos;
     }
 
-    public void exibeFichaTecnica() {
+    public void exibeFichaTecnica(){
         System.out.println("Nome do filme: " + nome);
         System.out.println("Ano de lançamento: " + anoDeLancamento);
     }
 
-    public void avalia(double nota) {
+    public void avalia(double nota){
         somaDasAvaliacoes += nota;
         totalDeAvaliacoes++;
     }
 
-    public double pegaMedia() {
+    public double pegaMedia(){
         return somaDasAvaliacoes / totalDeAvaliacoes;
     }
-
 
     @Override
     public int compareTo(Titulo outroTitulo) {
         return this.getNome().compareTo(outroTitulo.getNome());
+    }
+
+    @Override
+    public String toString() {
+        return "nome='" + nome + '\'' +
+                ", anoDeLancamento=" + anoDeLancamento;
+
     }
 }
